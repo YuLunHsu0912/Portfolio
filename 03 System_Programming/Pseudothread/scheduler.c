23aee890d5dd0d9e1e4eb7d6e5a9d4bfaa85220b
YuLunHsu0912 <65931399+YuLunHsu0912@users.noreply.github.com>
2021-03-03 15:37:52 +0800

    Add files via upload

---
+++ 03 System_Programming/Pseudothread/scheduler.c
@@ -0,0 +1,44 @@
+#include "threadutils.h"
+
+
+/*
+0. You should state the signal you received by:
+   printf('TSTP signal!\n') or printf('ALRM signal!\n')
+1. If you receive SIGALRM, you should reset alarm() by timeslice argument passed in ./main
+2. You should longjmp(SCHEDULER,1) once you're done
+*/
+void sighandler(int signo)
+{
+	if(signo==SIGTSTP)
+	{
+		printf("TSTP signal!\n"); 
+	}
+	if(signo==SIGALRM){
+		printf("ALRM signal!\n");
+		alarm(timeslice);
+	}
+	longjmp(SCHEDULER,1);
+}
+
+/*
+0. You are stronly adviced to make setjmp(SCHEDULER) = 1 for ThreadYield() case
+                                   setjmp(SCHEDULER) = 2 for ThreadExit() case
+1. Please point the Current TCB_ptr to correct TCB_NODE
+2. Please maintain the circular linked-list here
+*/
+void scheduler()
+{
+	int state=setjmp(SCHEDULER);
+	if(state==2){
+		if(Current->Prev==Current)
+			longjmp(MAIN,1);
+		Current->Prev->Next=Current->Next;		
+		Current->Next->Prev=Current->Prev;
+		Current=Current->Next;
+		longjmp(Current->Environment,1);
+	}else{
+		Current=Current->Next;
+		longjmp(Current->Environment,1);
+		}//need longjmp()
+	
+}
